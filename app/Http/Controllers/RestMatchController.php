dee27645ff1c702978d952797e0626cf3589c4c1
Dipesh Sharma <dipesh.ss19@gmail.com>
2023-04-21 00:37:28 +0545

    adding match logic

--- app/Http/Controllers/RestMatchController.php
+++ app/Http/Controllers/RestMatchController.php
@@ -45,8 +45,40 @@ class RestMatchController extends Controller
     }
     public  function  getMatchLeague($leagueId){
 
-        $match = League::with('teams.matches',)->find($leagueId);
-        return $match;
+        $league_id = $leagueId; // replace with your desired league id
+
+// Get all teams in the league
+        $teams = Team::where('league_id', $league_id)->get();
+// Get all matches for the teams in the league
+        $matches = Matche::with(
+            'homeTeam','awayTeam'
+        )->whereIn('home_team_id', $teams->pluck('id'))
+            ->orWhereIn('away_team_id', $teams->pluck('id'))
+            ->where('status', 1)
+            ->get();
+
+        $response = [];
+        foreach ($matches as $match) {
+            $response[] = [
+                'date' => $match->match_date_time,
+                'team1' => $match->homeTeam->name,
+                'team2' => $match->awayTeam->name,
+                'team1_logo' => $match->homeTeam->logo,
+                'team2_logo' => $match->awayTeam->logo,
+                'isOver' => ($match->status == 1) ? true : false,
+                'score' => [
+                    'team1' => $match->home_team_score,
+                    'team2' => $match->away_team_score,
+                ],
+                'city' => $match->location,
+                'stadium' => $match->stadium_name,
+            ];
+        }
+        return response()->json([
+            'success' =>true,
+            'data'  =>$response
+        ]);
+
 
     }
 
