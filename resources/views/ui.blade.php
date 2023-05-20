39c3f9a875b8eb791f577916c6e5749041df6f4f
Bhankundo Sharma <dipesh.ss19@gmail.com>
2023-05-20 06:44:57 +0000

    add server

--- resources/views/ui.blade.php
+++ resources/views/ui.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <title>Bhakundo.np</title>
+
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Bhakundo</title>
-    <script type="module" crossorigin src="/assets/index-fff84957.js"></script>
-    <link rel="stylesheet" href="/assets/index-44e330d7.css">
+    <script type="module" crossorigin src="/assets/index-46e70bf2.js"></script>
+    <link rel="stylesheet" href="/assets/index-f4520ab1.css">
   </head>
   <body>
     <div id="root"></div>
