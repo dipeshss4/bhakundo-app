116f0ee8070efdec63f5bf26366ccd93eed167d2
Rajesh Sharma <dude0@dude0.fritz.box>
2023-06-18 15:14:17 +0200

    Created a controller to add meta attribs on the server itself

--- resources/views/ui.blade.php
+++ resources/views/ui.blade.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head id="head">
     <title>Bhakundo.np</title>
 
     <meta charset="UTF-8" />
+    <meta name="description" content="{{$meta['desc']}}" />
+    <meta name="keywords" content="bhakundo" />
+    <meta name="title" content="{{$meta['title']}}" />
+    <meta name="url" content="{{$meta['url']}}" />
+    <meta name="og:url" content="{{$meta['url']}}" />
+    <meta name="og:type" content="{{$meta['type']}}" />
+    <meta name="og:title" content="{{$meta['title']}}" />
+    <meta name="og:description" content="{{$meta['desc']}}" />
+    <meta name="og:image" content="{{$meta['image']}}" />
+  
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script type="module" crossorigin src="/assets/index-46e70bf2.js"></script>
-    <link rel="stylesheet" href="/assets/index-f4520ab1.css">
+    <script type="module" crossorigin src="/assets/index-76d90a64.js"></script>
+    <link rel="stylesheet" href="/assets/index-2492fa42.css">
   </head>
   <body>
     <div id="root"></div>
